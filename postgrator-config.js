651f5966377ff162c7ac35fa335100a82479fd15
Jonathan Graziani <jpgraziani@gmail.com>
2021-02-24 21:25:25 -0700

    config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,29 +1,11 @@
-require("dotenv").config();
-const url = require("url");
-console.log("url ", url);
-
-let host;
-let port;
-let database;
-let username;
-
-if (process.env.DATABASE_URL) {
-  const dbUrl = url.parse(process.env.DATABASE_URL);
-  host = dbUrl.hostname;
-  port = dbUrl.port;
-  database = dbUrl.pathname.replace(/^\//, "");
-  [username] = dbUrl.auth;
-}
-
+require('dotenv').config();
+// const pg = require('pg');
+// pg.defaults.ssl = process.env.NODE_ENV === "production";
 
 module.exports = {
-  migrationDirectory: "migrations",
-  driver: "pg",
-  host,
-  port,
-  database,
-  username,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
 };
